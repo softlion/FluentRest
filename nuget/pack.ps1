f49c2aa482efda322c27fbf4cb36aef07799a71c
Benjamin Mayrargue <benjamin@vapolia.fr>
2022-01-12 23:22:22 +0100

    nuget pack

---
+++ nuget/pack.ps1
@@ -0,0 +1,25 @@
+#the "dotnet pack" command won't work for xamarin apps. Use desktop msbuild instead.
+if ($IsMacOS) {
+    $msbuild = "msbuild"
+} else {
+    $vswhere = 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe'
+    $msbuild = & $vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+    $msbuild = join-path $msbuild 'MSBuild\Current\Bin\MSBuild.exe'
+}
+
+#####################
+#Build release config
+$version="1.0.0"
+$versionSuffix=""
+$nugetVersion="$version$versionSuffix"
+#$versionSuffix=".$env:BUILD_NUMBER" 
+
+cd $PSScriptRoot
+del *.nupkg
+& $msbuild "..\FluentRest\FluentRest.sln" /restore /p:Configuration=Release /p:Platform="Any CPU" /p:Version="$version" /p:VersionSuffix="$versionSuffix" /p:Deterministic=false /p:PackageOutputPath="$PSScriptRoot" /p:IsHotRestartBuild=true --% /t:Clean;Build;Pack
+if ($lastexitcode -ne 0) { exit $lastexitcode; }
+del FluentRest.SourceGenerator.*.nupkg
+
+####################
+# PUSH
+dotnet nuget push "Softlion.FluentRest.$nugetVersion.nupkg" 
