9d1519d14a33996adeb62b07a8268eec0ea8949c
Benjamin Mayrargue <benjamin@vapolia.fr>
2022-01-17 10:44:45 +0100

    set better defaults

--- FluentRest/FluentRest/Http/Configuration/SystemTextJsonSerializer.cs
+++ FluentRest/FluentRest/Http/Configuration/SystemTextJsonSerializer.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using FluentRest.Rest.Configuration;
 
 namespace FluentRest.Http.Configuration
 {
 	public class SystemTextJsonSerializer : ISerializer
 	{
-		public static ISerializer Default { get; } = new SystemTextJsonSerializer(new JsonSerializerOptions(JsonSerializerDefaults.General));
-
+		private static readonly JsonSerializerOptions? DefaultOptions;
+		public static ISerializer Default { get; }
 		private readonly JsonSerializerOptions? options;
 
-		public SystemTextJsonSerializer(JsonSerializerOptions? options = null) => this.options = options;
+		static SystemTextJsonSerializer()
+		{
+			DefaultOptions = new JsonSerializerOptions(JsonSerializerDefaults.General)
+			{
+				PropertyNameCaseInsensitive = true,
+				DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+			};
+			Default = new SystemTextJsonSerializer(DefaultOptions);
+		}
+
+		public SystemTextJsonSerializer(JsonSerializerOptions? options = null) => this.options = options ?? DefaultOptions;
 
 		public string Serialize(object obj) => JsonSerializer.Serialize(obj, options);
 		public T? Deserialize<T>(string s) => JsonSerializer.Deserialize<T>(s, options);
